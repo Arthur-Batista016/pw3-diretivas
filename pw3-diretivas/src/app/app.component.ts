48f418d4c4d50f7f3a0e916b7152f1a3ab3ef8fa
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2025-04-15 11:00:50 -0300

    feat: @if adicinado e componente swtich criado

--- pw3-diretivas/src/app/app.component.ts
+++ pw3-diretivas/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IfComponent } from './if/if.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,IfComponent],
+  imports: [RouterOutlet,IfComponent,SwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
